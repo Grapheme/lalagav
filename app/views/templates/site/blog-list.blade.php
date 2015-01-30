75ea489bd103b1ac631c95bb7c5883716db1d89e
sngrl <reserved@mail.ru>
2015-01-30 19:33:30 +0300

    fix

--- app/views/templates/site/blog-list.blade.php
+++ app/views/templates/site/blog-list.blade.php
@@ -75,9 +75,10 @@ $blog = DicLib::loadImages($blog, 'image_id');
                     <div class="clrfx"></div>
                 </div>
             @endforeach
-        @endif
 
-        {{ $blog->links() }}
+            {{ $blog->links() }}
+
+        @endif
 
         <div class="paginator">
             <a href="?page-prev"><img src="{{ Config::get('site.theme_path') }}/images/ico-paginator-left.svg"></a>
